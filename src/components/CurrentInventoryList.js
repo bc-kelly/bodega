8247409ccd679e75a02e058dd6e8eb35d7f5f40c
Bridget Kelly <bckelly197@gmail.com>
2022-03-02 11:56:36 -0500

    stuck

--- src/components/CurrentInventoryList.js
+++ src/components/CurrentInventoryList.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import InventoryItemCard from './InventoryItemCard'
 
 function CurrentInventoryList() {
+    const [onInventory, setOnInventory] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8001/inventory')
+        .then(resp => resp.json())
+        .then(bodegaData => {
+            console.log(bodegaData);
+            setOnInventory(bodegaData);
+           
+        })
+    }, [])
+
 
     return(
         <div id="current-inventory">
             <h2>Current Inventory</h2>
             <div>
-                {/** Put inventory items here */}
+               {onInventory.map((item) => {
+                   return (
+                    <InventoryItemCard
+                        key={item.id}
+                        itemName={item.name}
+                        itemImage={item.image}
+                        itemPrice={item.price}
+                    />
+                   )
+                })
+            }
             </div>
         </div>
     );
 }
 
 export default CurrentInventoryList;
+
+{/* check - have fetched the items
+kind og check - need to get them onto the page
+to do this have to get them into inventoryItemCard
+have to pass the name image and price as props to the card
+now that they're on the page i need to get their name and images to show up
+something is wrong with my props 
+ */}
--- src/components/InventoryManger.js
+++ src/components/InventoryManger.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import CurrentInventoryList from "./CurrentInventoryList";
 import ReorderInventoryList from "./ReorderInventoryList"
 
 function InventoryManager() {
+    const [onAddItem, setOnAddItem] = useState('')
+
+
+    // handleAddToInventory () {
+    //     setOnAddItem()
+    // }
 
     return(
         <div className="container">
-            <CurrentInventoryList />
-            <ReorderInventoryList />
+            <CurrentInventoryList 
+                onClick={(event) => {setOnAddItem(event.target.id)}}
+            />
+            <ReorderInventoryList
+                addedItems={onAddItem}
+            />
         </div>
     );
 }
